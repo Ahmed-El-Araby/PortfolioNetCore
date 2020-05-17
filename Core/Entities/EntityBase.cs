167d32f29f38f624295e05f9da0b98497a5f2483
Ahmed-El-Araby <ahmad.met.5s@gmail.com>
2020-05-17 15:54:22 -0700

    Add project files.

---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class EntityBase
+    {
+        public Guid Id { set; get; }
+    }
+    public class Owner: EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profil { get; set; }
+        public string Avatar { get; set; }
+    }
+    public  class Address: EntityBase
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Coutry { get; set; }
+    }
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { set; get; }
+        public string Desctiption { set; get; }
+        public int ImageUrl { set; get; }
+        public Address address { get; set; }
+        
+    }
+    
+}
